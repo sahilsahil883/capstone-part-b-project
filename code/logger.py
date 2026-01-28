ff180e9c442cc7f4db1167477d2499fdc3b6a120
sahilsahil883 <sahilchodry333@gmail.com>
2026-01-28 10:03:14 +0530

    Update logger.py

--- code/logger.py
+++ code/logger.py
@@ -1,1 +1,8 @@
+import datetime
 
+def log_event(username, action):
+    timestamp = datetime.datetime.now()
+    log_entry = f"{timestamp} | User: {username} | Action: {action}"
+    
+    with open("activity.log", "a") as log_file:
+        log_file.write(log_entry + "\n")
